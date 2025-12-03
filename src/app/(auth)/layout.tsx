a4a50b43d03509f9b4758f4f67a3a3c4aa602b5e
Edwin Aguiar <faubriciosanchez1@gmail.com>
2025-12-03 12:15:37 +0000

    Try fixing this error: `Runtime Error: Error: useFirebase must be used w

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { KonimPayLogo } from '@/components/icons';
+import { FirebaseClientProvider } from '@/firebase';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-4">
-      <div className="w-full max-w-sm">
-        <div className="mb-8 flex justify-center text-primary">
-          <KonimPayLogo className="h-auto w-40" />
+    <FirebaseClientProvider>
+      <div className="flex min-h-screen flex-col items-center justify-center p-4">
+        <div className="w-full max-w-sm">
+          <div className="mb-8 flex justify-center text-primary">
+            <KonimPayLogo className="h-auto w-40" />
+          </div>
+          {children}
         </div>
-        {children}
       </div>
-    </div>
+    </FirebaseClientProvider>
   );
 }
