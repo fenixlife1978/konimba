9f850d2220aecb67772120208b599e610fd7d2a5
Edwin Aguiar <faubriciosanchez1@gmail.com>
2025-12-05 15:41:16 +0000

    configura la inicializacion de firebase y las apikeys:     // Import the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-1531665135-d603d",
-  "appId": "1:892376341822:web:9913567e474fa7eb2aa06f",
-  "apiKey": "AIzaSyB6OQZvVklwXyjL0Zy1pcO8JwQvi2jfvH0",
-  "authDomain": "studio-1531665135-d603d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "892376341822"
+  apiKey: "AIzaSyB6OQZvVklwXyjL0Zy1pcO8JwQvi2jfvH0",
+  authDomain: "studio-1531665135-d603d.firebaseapp.com",
+  projectId: "studio-1531665135-d603d",
+  storageBucket: "studio-1531665135-d603d.firebasestorage.app",
+  messagingSenderId: "892376341822",
+  appId: "1:892376341822:web:9913567e474fa7eb2aa06f"
 };
